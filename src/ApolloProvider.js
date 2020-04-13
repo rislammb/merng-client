2a10ccec63680ed10569fe697e943382bd6f44b0
unknown <rislammb@gmail.com>
2020-04-13 18:22:12 +0600

    add proxy and change base uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,21 +7,21 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://floating-eyrie-46326.herokuapp.com',
 });
 
 const authLink = setContext(() => {
   const token = localStorage.getItem('jwtToken');
   return {
     headers: {
-      Authorization: token ? `Bearer ${token}` : ''
-    }
+      Authorization: token ? `Bearer ${token}` : '',
+    },
   };
 });
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 export default (
